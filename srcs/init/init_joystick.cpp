d58d651a2a3f61db93c934d37f3a0f2ac91259f2
jose5556 <josefilmeneses@gmail.com>
2025-12-03 13:16:17 +0000

    Improve joystick initialization comments for clarity

--- srcs/init/init_joystick.cpp
+++ srcs/init/init_joystick.cpp
@@ -1,14 +1,18 @@
 #include "../../include/carControl.h"
 
 SDL_Joystick*	initJoystick() {
+
+	// Initialize SDL joystick subsystem
 	if (SDL_Init(SDL_INIT_JOYSTICK) < 0)
 		throw (InitException("Failed to initialize SDL joystick"));
 
+	// Check how many joysticks are connected
 	if (SDL_NumJoysticks() <= 0) {
 		SDL_Quit();
 		throw (InitException("No joystick detected"));
 	}
 
+	// Open the first available joystick (index 0)
 	SDL_Joystick *joystick = SDL_JoystickOpen(CONTROLLER_0);
 	if (joystick == nullptr) {
 		SDL_Quit();
