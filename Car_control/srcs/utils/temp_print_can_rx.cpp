1ad6a3d02199eca07b9a68aa19ca3b63391e9941
jose5556 <josefilmeneses@gmail.com>
2026-01-19 15:21:52 +0000

    refactor: update testing configuration and improve readCan function

--- Car_control/srcs/utils/temp_print_can_rx.cpp
+++ Car_control/srcs/utils/temp_print_can_rx.cpp
@@ -2,18 +2,13 @@
 
 // Temp function just to print receiving vaues for now
 void	readCan(const std::unique_ptr<CANController> &can) {
+    can_frame	rx;
+    memset(&rx, 0, sizeof(can_frame));
 
-	can_frame	rx;
-	memset(&rx, 0, sizeof(can_frame));
-
-	if (can->receiveFrame(&rx) == 0) {  // 0 = sucesso
-		std::cout << "ID: 0x" << std::hex << rx.can_id 
-				<< " DLC: " << std::dec << (int)rx.can_dlc 
-				<< " Data: ";
-		for (int i = 0; i < rx.can_dlc; i++) {
-			std::cout << std::hex << std::setw(2) << std::setfill('0') 
-				<< (int)rx.data[i] << " ";
-		}
-		std::cout << std::dec << std::endl;
-	}
+    if (can->receiveFrame(&rx) == 0) {  // 0 = sucesso
+        if (rx.can_dlc >= 2) {
+            uint16_t rpm = (rx.data[0] << 8) | rx.data[1];
+            std::cout << "Raw RPM: " << rpm << std::endl;
+        }
+    }
 }
