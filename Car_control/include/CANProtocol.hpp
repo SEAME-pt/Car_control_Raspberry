5d6498fdfae285259acdde115b4831cd815d2828
jose5556 <josefilmeneses@gmail.com>
2026-02-04 12:33:18 +0000

    refactor: update CANProtocol to include heartbeat signal and improve namespace consistency

--- Car_control/include/CANProtocol.hpp
+++ Car_control/include/CANProtocol.hpp
@@ -2,20 +2,28 @@
 
 #include "CANController.hpp"
 
+uint16_t rpmToSpeedMps(uint16_t rpm);
+
 /**
  * @file CANProtocol.hpp
  * @brief CAN IDs and protocol helper functions for sending vehicle commands.
  */
 
 /**
- * @namespace CANID
- * @brief Predefined CAN IDs used by the vehicle communication protocol.
+ * @namespace CANSENDID
+ * @brief Predefined CAN IDs used by the vehicle communication protocol to send messages.
  */
-namespace CANID {
+namespace CANSENDID {
 	constexpr uint16_t	EMERGENCY_BRAKE 	= 0x100;	/**< Emergency brake command (max priority) */
 	constexpr uint16_t	COMMAND_THROTTLE	= 0x101;	/**< Throttle command (high priority) */
 	constexpr uint16_t	COMMAND_STEERING	= 0x102;	/**< Steering command (high priority) */
+	constexpr uint16_t	HEARTBEAT			= 0x103;	/**< Heartbeat signal (keep-alive) */
 };
+
+namespace CANRECEIVERID {
+	constexpr uint16_t	SPEEDRPMSTM32			= 0x200;
+	constexpr uint16_t	BATTERYSTM32			= 0x201;
+}
 
 /**
  * @namespace CANProtocol
@@ -32,7 +40,7 @@ namespace CANProtocol {
 	inline void sendEmergencyBrake(CANController& can, bool active) {
 
 		int8_t data = active ? 0xF : 0x00;
-		can.sendFrame(CANID::EMERGENCY_BRAKE, &data, 1);
+		can.sendFrame(CANSENDID::EMERGENCY_BRAKE, &data, 1);
 	}
 
 	/**
@@ -44,9 +52,9 @@ namespace CANProtocol {
 	inline void sendThrottleCommand(CANController& can, int16_t throttle) {
 
 		int8_t data[2];
-    	data[0] = static_cast<int8_t>(throttle & 0xFF);         // Low byte
-    	data[1] = static_cast<int8_t>((throttle >> 8) & 0xFF);  // High byte
-		can.sendFrame(CANID::COMMAND_THROTTLE, data, 2);
+		data[0] = static_cast<int8_t>(throttle & 0xFF);         // Low byte
+		data[1] = static_cast<int8_t>((throttle >> 8) & 0xFF);  // High byte
+		can.sendFrame(CANSENDID::COMMAND_THROTTLE, data, 2);
 	}
 
 	/**
@@ -58,8 +66,34 @@ namespace CANProtocol {
 	inline void sendSteeringCommand(CANController& can, int16_t steering) {
 		
 		int8_t data[2];
-    	data[0] = static_cast<int8_t>(steering & 0xFF);         // Low byte
-    	data[1] = static_cast<int8_t>((steering >> 8) & 0xFF);  // High byte
-		can.sendFrame(CANID::COMMAND_STEERING, data, 2);
+		data[0] = static_cast<int8_t>(steering & 0xFF);         // Low byte
+		data[1] = static_cast<int8_t>((steering >> 8) & 0xFF);  // High byte
+		can.sendFrame(CANSENDID::COMMAND_STEERING, data, 2);
+	}
+
+	inline uint16_t	receiveSpeedValues(CANController& can) {
+
+		can_frame	rx;
+		memset(&rx, 0, sizeof(can_frame));
+		
+		if (can.receiveFrame(&rx) == 0) {
+			if (rx.can_dlc >= 2) {	//&& rx.can_id == CANRECEIVERID::SPEEDRPMSTM32
+				uint16_t rpm = (rx.data[0] << 8) | rx.data[1];
+				return (rpmToSpeedMps(rpm));
+			} else
+				std::cout << "boi" << std::endl; 
+		}
+		return (0);
+	}
+
+	/**
+	 * @brief Sends a heartbeat signal over CAN.
+	 *
+	 * @param can Reference to an initialized CANController
+	 */
+	inline void sendHeartbeat(CANController& can) {
+
+    	int8_t data = 0xAA;  // Heartbeat signature
+    	can.sendFrame(CANSENDID::HEARTBEAT, &data, 1);
 	}
 }
