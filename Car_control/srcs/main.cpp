97ca091630db03fe135d1b6610198cd769b91e22
team1 <team1@seame.pt>
2026-02-09 08:42:32 +0000

    fixing main

--- Car_control/srcs/main.cpp
+++ Car_control/srcs/main.cpp
@@ -7,14 +7,14 @@ int	main(int argc, char *argv[]) {
 
 	t_carControl carControl = initCarControl(argc, argv);
 	if (carControl.exit)
-		return (1)
+		return (1);
 	try {
 		if (!carControl.manual) {
 			std::cout << "Autonomous mode chosed, initiating ai..." << std::endl;
 			autonomousLoop(carControl);
 		} else {
 			std::cout << "Manual mode chosed, initiating joystick..." << std::endl;
-			manualLoop(&carControl);without input_event struct too nothing, bare everything
+			manualLoop(&carControl); //without input_event struct too nothing, bare everything
 		}
 	} catch (const std::exception &err) {
 		std::cerr << err.what() << std::endl;
